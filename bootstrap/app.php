94e956aaf29faf8ae65bf2cdd8a49fdd44ee13e9
alzaarh <alza.arhane97@gmail.com>
2019-07-15 21:07:27 +0430

    asd

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -56,13 +56,12 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\RateLimits::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\RateLimits::class
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Auth::class,
-    'throttle' => App\Http\Middleware\RateLimits::class,
 ]);
 
 /*
