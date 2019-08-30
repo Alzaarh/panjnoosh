40df4bf0516ff6b18f3f3f87c339328de923cea5
alzaarh <alza.arhane97@gmail.com>
2019-08-30 16:01:34 +0430

    city@update, city@delete

--- app/Http/Controllers/CitiesController.php
+++ app/Http/Controllers/CitiesController.php
@@ -50,6 +50,32 @@ class CitiesController extends Controller {
         return response()->json(['message' => 'file upload error'], 400);
     }
 
+    public function update(Request $request, $id) {
+        $city = City::findOrFail($id);
+
+        $this->validateUpdate($request);
+
+        if ($request->input('title')) {
+            $city->title = $request->input('title');
+        }
+
+        if ($request->input('stateId')) {
+            $city->state_id = $request->input('stateId');
+        }
+
+        $city->save();
+
+        return new CityResource($city);
+    }
+
+    public function delete(Request $request, $id) {
+        $city = City::findOrFail($id);
+
+        $city->delete();
+
+        return response()->json(['message' => 'city deleted'], 200);
+    }
+
     private function validateSearchTerm($request) {
         $this->validate($request, [
             'search' => 'required|string|max:30'
@@ -67,6 +93,14 @@ class CitiesController extends Controller {
                     $fail($attribute . 'is invalid');
                 }
             }]
+        ]);
+    }
+
+    private function validateUpdate($request) {
+        $this->validate($request, [
+            'string|max:255',
+
+            'stateId' => 'numeric|exists:states,id'
         ]);
     }
 }
